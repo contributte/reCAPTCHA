65fd3e985df877278cb3a8eac5fe20ec0eec6495
Milan Felix Šulc <rkfelix@gmail.com>
2018-04-06 21:00:57 +0200

    Tests: cleanup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,29 +1,13 @@
 <?php
 
-use Tester\Environment;
-use Tester\Helpers;
+use Ninjify\Nunjuck\Environment;
 
 if (@!include __DIR__ . '/../vendor/autoload.php') {
-    echo 'Install Nette Tester using `composer update --dev`';
-    exit(1);
+	echo 'Install Nette Tester using `composer update --dev`';
+	exit(1);
 }
 
 // Configure environment
-Environment::setup();
-date_default_timezone_set('Europe/Prague');
-
-// Create temporary directory
-define('TEMP_DIR', __DIR__ . '/tmp/' . getmypid());
-@mkdir(dirname(TEMP_DIR));
-
-// Purge temporary directory
-Helpers::purge(TEMP_DIR);
-
-/**
- * @param Closure $function
- * @return void
- */
-function test(\Closure $function)
-{
-    $function();
-}
+Environment::setupTester();
+Environment::setupTimezone();
+Environment::setupVariables(__DIR__);
--- tests/tester.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-%CD%\..\vendor\bin\tester.bat %CD%\cases -c php-win.ini -s -j 40 -log %CD%\tester.log --coverage coverage.html --coverage-src %CD%\..\src %*
-rmdir %CD%\tmp /Q /S
