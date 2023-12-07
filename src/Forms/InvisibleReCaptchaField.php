6e617d5aed6e86eb706fc39bab50faf1082d50f6
Pavel W <pavel.weirich@makeart.cz>
2023-12-07 12:49:43 +0100

    Used LogicException instead of custom class

--- src/Forms/InvisibleReCaptchaField.php
+++ src/Forms/InvisibleReCaptchaField.php
@@ -50,7 +50,7 @@ class InvisibleReCaptchaField extends HiddenField
 	public function setMinimalScore(float $score): self
 	{
 		if ($score < 0 || $score > 1) {
-			throw new InvalidScoreException('Minimal score expects to be in range 0..1 (1.0 is very likely a good interaction, 0.0 is very likely a bot).');
+			throw new \LogicException('Minimal score expects to be in range 0..1 (1.0 is very likely a good interaction, 0.0 is very likely a bot).');
 		}
 
 		$this->provider->setMinimalScore($score);
